690075707a561193d64e4611e3dcc4a2ee0347a8
Timothy W. Hilton <thilton@ucmerced.edu>
2016-06-01 14:25:55 -0700

    initial commit
    - this version:
      - parses all CLM_F05_g16 50-year spinup monthly files
      - locates the CLM grid cell containing Santa Cruz, CA, USA
      - plots a map of global GPP for one arbitrary month and plots Santa
        Cruz on that map just make sure everything shows up in the right
        place
      - plots a time series of July Santa Cruz GPP for all 50 spinup years
        - I could make myself believe that there's a decreasing trend in
          the last five years or so of GPP
    - TODO:
      - plot variables other than GPP
      - plot months other than July

---
+++ clm_domain.py
@@ -0,0 +1,160 @@
+"""a set of tools useful for parsing and returning descriptions of the
+STEM domain (grid cell longitude and latitude coordinates, grid cell
+vertical coordinates), and also for some domain-oriented calculations
+(is a point inside or outside of the domain, etc.)
+"""
+
+import os
+import os.path
+import matplotlib
+import numpy as np
+import warnings
+import netCDF4
+import stem_pytools.STEM_parsers as sp
+from mpl_toolkits import basemap
+from scipy.spatial import cKDTree
+
+
+class Location(object):
+    """class to contain the location of a point.  Contains user-accessible
+    fields lon and lat.
+    """
+    def __init__(self, lon, lat):
+        self.lon = lon
+        self.lat = lat
+
+
+class CLM_Domain(object):
+    """class to contain CLM domain attributes.
+    """
+    def __init__(self, fname):
+        """Populates fields: fname, lon, lat, topo, area
+        ARGS:
+        fname: CLM output file from which to read lon, lat
+        """
+        self.fname = fname
+        nc = netCDF4.Dataset(fname)
+        self.lon1D = nc.variables['lon'][:]
+        self.lat1D = nc.variables['lat'][:]
+        self.topo =  nc.variables['topo'][:]
+        self.area = nc.variables['area'][:]
+        self.lon, self.lat = np.meshgrid(self.lon1D, self.lat1D)
+        return(None)
+
+    def get_lat(self):
+        """return the CLM latitude grid"""
+        return(self.STEM_lat)
+
+    def get_lon(self):
+        """return the CLM longitude grid"""
+        return(self.STEM_lon)
+
+    def get_topo(self):
+        """return the CLM topo grid"""
+        return(self.topo)
+
+    def find_nearest_xy(self, lon, lat):
+        """find indices of the nearest grid point for a set of lon, lat points.
+
+        Given a set of arbitrary (lon, lat) positions, find the horizontal
+        (x, y) CLM grid indices of the nearest CLM grid cell to each
+        position.
+
+        PARAMETERS
+        ----------
+        lon, lat: ndarray; of arbitrary longitudes and latitudes.  Must
+           contain the same number of elements.
+
+        RETURNS:
+        an N-element tuple of X and Y indices, one index per observation.
+            The indices are the closest point on the CLM grid to each
+            point in (lon, lat).  N is therefore equal to the number of
+            dimensions in lon and lat.
+
+        """
+        # convert spherical lon, lat coordinates to cartesian coords. Note
+        # that these x,y,z are 3-dimensional cartesian coordinates of
+        # positions on a sphere, and are thus different from the x,y,z
+        # *indices* of the STEM grid.
+        x, y, z = lon_lat_to_cartesian(lon, lat)
+        xs, ys, zs = lon_lat_to_cartesian(self.lon, self.lat)
+
+        # use a K-dimensional tree to find the nearest neighbor to (x,y,z)
+        # from the points within (xs, ys, zs).  A KD tree is a data
+        # structure that allows efficient queries of K-dimensional space (K
+        # here is 3).
+        tree = cKDTree(np.dstack((xs.flatten(),
+                                  ys.flatten(),
+                                  zs.flatten())).squeeze())
+
+        d, inds = tree.query(
+            np.dstack((x, y, z)).squeeze(), k=1)
+
+        return(np.unravel_index(inds, self.lon.shape))
+
+
+
+def lon_lat_to_cartesian(lon, lat, R=1):
+    """
+    Convert spherical coordinates to three-dimensional Cartesian
+    coordinates.
+
+    calculates three dimensional cartesian coordinates (x, y, z) for
+    specified longitude, latitude coordinates on a sphere with radius
+    R.  Written and posted at earthpy.org by Oleksandr Huziy.
+    http://earthpy.org/interpolation_between_grids_with_ckdtree.html
+    accessed 19 Mar 2014 by Timothy W. Hilton.
+
+    PARAMETERS
+    ==========
+    lon; np.ndarray: longitude values
+    lat; np.ndarray: latitude values
+    R: scalar; radius of the sphere.
+
+    RETURNS
+    =======
+    three element tuple containing X, Y, and Z, one element per
+    lon,lat pair.
+    """
+    lon_r = np.radians(lon)
+    lat_r = np.radians(lat)
+
+    x = R * np.cos(lat_r) * np.cos(lon_r)
+    y = R * np.cos(lat_r) * np.sin(lon_r)
+    z = R * np.sin(lat_r)
+    return x, y, z
+
+
+def demo():
+    """Demonstrates using the domain module to find the indices of a
+    couple of key locations on a 1 degree by 1 degree Northern
+    Hemisphere grid that are inside the area specifed by a STEM topo
+    file.
+
+    """
+    domain = CLM_Domain(fname=os.path.join('/', 'global',
+                                           'cscratch1', 'sd',
+                                           'twhilton',
+                                           'archive',
+                                           'CLM_f05_g16',
+                                           'lnd',
+                                           'hist',
+                                           'CLM_f05_g16.clm2.h0.0050-01.nc'))
+    santacruz = Location((122.03089741, ), (36.9741, ))
+    idx = domain.find_nearest_xy(np.array((santacruz.lon)),
+                                 np.array((santacruz.lat)))
+    return idx
+    # mymap = domain.get_mapobj()
+    # mymap.drawcoastlines()
+    # mymap.plot(domain.bnd_lon, domain.bnd_lat, latlon=True)
+    # mymap.scatter(sib_lon,
+    #               sib_lat,
+    #               latlon=True,
+    #               c='gray',
+    #               facecolors='none',
+    #               marker='o')
+    # mymap.scatter(sib_lon[in_STEM_domain],
+    #               sib_lat[in_STEM_domain],
+    #               latlon=True,
+    #               c='red',
+    #               marker='x')
---
+++ spinup_diagnostics.py
@@ -0,0 +1,135 @@
+"""tools to check up on CLM spinups.  Currently it is mostly geared
+toward parsing time series of a few key variables and making sure the
+spinup achieved steady state """
+
+import netCDF4
+import numpy as np
+import os
+import glob
+import matplotlib.pyplot as plt
+
+from clm_domain import CLM_Domain, Location
+
+class CLM_spinup_analyzer(object):
+    """class to read CLM spinup output and plot key variables
+    """
+
+    def __init__(self,
+                 data_dir,
+                 CASE):
+        """
+        Args:
+        data_dir: full path to the directory containing the spinup run data
+        CASE (string): the "case" name of the CESM run
+        """
+        self.data_dir = data_dir
+        self.CASE = CASE
+        self.gather_filenames()
+
+    def gather_filenames(self):
+        """gather all netCDF (*.nc) filenames from self.data_dir and place
+        them in self.data_filenames
+        """
+        self.all_files = sorted(glob.glob(os.path.join(self.data_dir, "*.nc")))
+
+    def build_fname(self, year, month):
+        """assemble the filename for the
+        """
+        return "{}.clm2.h0.{:04d}-{:02d}.nc".format(self.CASE, year, month)
+
+    def parse_var(self,
+                  varname,
+                  lon_idx=None,
+                  lat_idx=None,
+                  mask_missing=True,
+                  verbose=False):
+        """parse a specified variable from all spinup files.
+
+        Optionally, restrict parsing to a specified grid cell.
+
+        Args:
+        varname (string): name of the netCDF variable to parse.  Must
+            be present in all files in self.data_filenames
+        lon_idx (integer): optional; if specified only a single grid
+            cell will be parsed.  Ignored if lat_idx is not also specified.
+        lat_idx (integer): optional; if specified only a single grid
+            cell will be parsed
+        mask_missing ({True}|False): if True, check the requested
+            netCDF variable for an attribute named "missing_value".
+            If present, parse it and return a numpy masked array with
+            values equal (within floating point tolerance) to
+            missing_value masked.
+        verbose (True|{False}): if True, display a status message as
+            each file is read
+
+        It is assumed that CLM output files contain one and only one
+        timestep.
+        """
+
+        nt = len(self.all_files)
+        nlon = 1
+        nlat = 1
+        if (lon_idx is None) or (lat_idx is None):
+            nc = netCDF4.Dataset(self.all_files[0])
+            nlon = len(nc.dimensions['lon'])
+            nlat = len(nc.dimensions['lat'])
+            lon_idx = np.arange(nlon)
+            lat_idx = np.arange(nlat)
+            nc.close()
+        data = np.zeros([nt, nlat, nlon])
+
+        # TODO: intialize array for time here
+
+        missing_value = None
+        for i in range(nt):
+            if verbose:
+                print 'parsing {}'.format(os.path.basename(self.all_files[i]))
+            nc = netCDF4.Dataset(self.all_files[i])
+            data[i, ...] = nc.variables[varname][0, lat_idx, lon_idx]
+            # TODO: parse time here
+            try:
+                missing_value = nc.variables[varname].missing_value
+            except AttributeError:
+                if verbose:
+                    print "no missing_value for {} in {}".format(
+                        os.path.basename(self.all_files[i]), varname)
+            nc.close()
+        if missing_value is not None:
+            data = np.ma.masked_values(data, missing_value)
+        return data
+
+
+def parse_CLM_f05_g15():
+    """wrapper function to read data from CLM_f05_g16 spinup run
+    """
+    CLM_f05_g16 = CLM_spinup_analyzer(os.path.join('/', 'global',
+                                                   'cscratch1', 'sd',
+                                                   'twhilton',
+                                                   'archive',
+                                                   'CLM_f05_g16',
+                                                   'lnd',
+                                                   'hist'),
+                                      'CLM_f05_g16')
+    fpsn = CLM_f05_g16.parse_var('FPSN', verbose=True)
+    return (CLM_f05_g16, fpsn)
+
+
+
+if __name__ == "__main__":
+
+    CLM_f05_g16, fpsn = parse_CLM_f05_g15()
+    idx_jul = np.array([this_file.find('07.nc')
+                        for this_file in CLM_f05_g16.all_files]) > 0
+    domain_f05_g16 = CLM_Domain(CLM_f05_g16.all_files[0])
+    santacruz = Location((-122.03089741, ), (36.9741, ))
+    santacruz.clm_y, santacruz.clm_x = domain_f05_g16.find_nearest_xy(
+        santacruz.lon,
+        santacruz.lat)
+    fig, ax = plt.subplots()
+    ax.pcolormesh(fpsn[100, ...], cmap=plt.get_cmap('Greens'))
+    ax.scatter(santacruz.clm_x, santacruz.clm_y, c='black', marker='x', s=40)
+    fig, ax = plt.subplots()
+    ax.plot(fpsn[idx_jul, santacruz.clm_y, santacruz.clm_x])
+    ax.set_title('July Santa Cruz GPP (umol m-2 s-1)')
+    ax.set_xlabel('year of spinup')
+    ax.set_ylabel('GPP')
