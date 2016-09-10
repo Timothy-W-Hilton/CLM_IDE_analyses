2a80e4320fe2ebb23b02c14deb020bc1303cf96a
Timothy W. Hilton <thilton@ucmerced.edu>
2016-09-10 12:16:35 -0700

    implement pynco example
    - [pynco](https://github.com/nco/pynco) provides python bindings to
      netCDF operators (nco).
    - this example computes daily averages from hourly CLM output

---
+++ pynco_test.py
@@ -0,0 +1,27 @@
+import os
+import spinup_diagnostics as sd
+from nco import Nco
+from datetime import datetime
+
+if __name__ == "__main__":
+    data_dir = os.path.join(os.getenv('CSCRATCH'), 'archive',
+                            'CLM_f05_g16_control', 'lnd', 'hist')
+    ctl_clm_run = sd.CLM_spinup_analyzer(data_dir=data_dir,
+                                         CASE='CLM_f05_g16_control')
+    ctl_clm_run.gather_filenames(glob_pat='*h1*.nc')
+
+    nco = Nco()
+    # t0 = datetime.now()
+    # print "concatenating netcdf files"
+    # nco.ncrcat(input=ctl_clm_run.all_files,
+    #            options='-v FPSN',
+    #            output="out.nc")
+    # print "done concatenating (t = {})".format(datetime.now - t0)
+
+    # ncra --mro -O -F -d time,6,,12,3 -n 150,4,1 1850.nc 1850_2009_JJA.nc
+    t0 = datetime.now()
+    print "concatenating netcdf files"
+    nco.ncra(input=ctl_clm_run.all_files,
+             output="out_avg.nc",
+             options="--mro -O -F -d time,1,,24,24 -v FPSN")
+    print "done concatenating (t = {})".format(datetime.now() - t0)
