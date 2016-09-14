994c18b44dfad5566006a309f7e2cabcc2221601
Timothy W. Hilton <thilton@ucmerced.edu>
2016-09-14 10:45:35 -0700

    add timing printouts, fix empty file list bug
    - print start time as well as elapsed time at completion of ncra
    - If the input data file directory is empty, nco.ncra hangs!  Add a
      check to make sure there are some input files

--- pynco_test.py
+++ pynco_test.py
@@ -20,8 +20,11 @@ if __name__ == "__main__":
 
     # ncra --mro -O -F -d time,6,,12,3 -n 150,4,1 1850.nc 1850_2009_JJA.nc
     t0 = datetime.now()
-    print "concatenating netcdf files"
-    nco.ncra(input=ctl_clm_run.all_files,
-             output="out_avg.nc",
-             options="--mro -O -F -d time,1,,24,24 -v FPSN")
+    print "concatenating netcdf files (start: {})".format(
+        t0.strftime("%H:%M:%S"))
+    varname = "H2OSOI"
+    if len(ctl_clm_run.all_files) is not 0:
+        nco.ncra(input=ctl_clm_run.all_files,
+                 output="out_avg_{}.nc".format(varname),
+                 options="--mro -O -F -d time,1,,24,24 -v {}".format(varname))
     print "done concatenating (t = {})".format(datetime.now() - t0)
