d81db39b2e849fa2b885648c7cebc2c0d2479f18
Timothy W. Hilton <thilton@ucmerced.edu>
2016-09-30 14:49:39 -0700

    save pdf, convert mm/s to mm

--- IDE_postprocess.py
+++ IDE_postprocess.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
                                     'lnd', 'hist'),
                        'ide_redpcp')}
 
+    s_per_tstep = 60 * 60 * 6  # tsteps are 6 hours
     vars = {'RAIN': {}, 'QVEGE': {}, 'QVEGT': {}}
     for run_name, run in runs.iteritems():
         for this_var in vars.keys():
@@ -23,14 +24,19 @@ if __name__ == "__main__":
             vars[this_var][run_name] = run.parse_var(this_var,
                                                      lon_idx=locs[0].clm_x,
                                                      lat_idx=locs[0].clm_y)
+            vars[this_var][run_name].data *= s_per_tstep
+            vars[this_var][run_name].units = (
+                vars[this_var][run_name].units.replace('/s', ''))
 
-    fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(10, 6))
+    fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(7, 10))
     for this_var, this_ax in zip(vars.values(), ax):
         this_ax.plot(this_var['ctl'].data.squeeze(), 'b-', label='CTL')
         this_ax.plot(this_var['ide'].data.squeeze(), 'k--', label='IDE')
         this_ax.set_ylabel('{} ({})'.format(this_var['ctl'].varname,
                                             this_var['ctl'].units))
-        this_ax.set_xlabel('day of simulation')
+    ax[-1].set_xlabel('day of simulation')
     ax[0].set_title('{}'.format(locs[0].name))
     ax[0].legend(loc='best')
-    plt.show()
+    fig.tight_layout()
+    fig.savefig(os.path.join(os.getenv('CSCRATCH'), 'YL.pdf'))
+    # plt.show()
