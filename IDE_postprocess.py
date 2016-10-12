da11ddc2750e79fe69ea48b433161a5165e5aff2
Timothy W. Hilton <thilton@ucmerced.edu>
2016-10-12 08:24:38 -0700

    remove old code

--- IDE_postprocess.py
+++ IDE_postprocess.py
@@ -99,54 +99,6 @@ class MonthlyParser(object):
     def get_moy(self):
         return self.moy
 
-
-
-# if __name__ == "__main__":
-#     d_archive = os.path.join(os.getenv('CSCRATCH'), 'monthly_means')
-#     locs = IDE_locations.CLMf05g16_get_spatial_info()
-#     d = locs[0]
-#     locs = locs[1:]
-#     runs = {'ctl': CSA(os.path.join(d_archive, '),
-#                        'ide_ctl'),
-#             'ide': CSA(os.path.join(d_archive, 'IDE_redpcp',
-#                                     'lnd', 'hist'),
-#                        'ide_redpcp')}
-#     mavg = []
-
-#     s_per_tstep = 60 * 60 * 6  # tsteps are 6 hours
-
-#     data = {}
-#     for this_loc in locs:
-#         vars = {'RAIN': {}, 'QVEGE': {}, 'QVEGT': {}}
-#         for run_name, run in runs.iteritems():
-#             for this_var in vars.keys():
-#                 run.gather_filenames(glob_pat="*h2*.nc")
-#                 vars[this_var][run_name] = run.parse_var(
-#                     this_var,
-#                     lon_idx=this_loc.clm_x,
-#                     lat_idx=this_loc.clm_y)
-#                 vars[this_var][run_name].data *= s_per_tstep
-#                 vars[this_var][run_name].units = (
-#                     vars[this_var][run_name].units.replace('/s', ''))
-#         data[this_loc.name] = vars
-
-#     # for this_site, this_data in data.items():
-#     #     fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(7, 10))
-#     #     for this_var, this_ax in zip(this_data.values(), ax):
-#     #         this_ax.plot(this_var['ctl'].data.squeeze(), 'b-', label='CTL')
-#     #         this_ax.plot(this_var['ide'].data.squeeze(), 'k--', label='IDE')
-#     #         this_ax.set_ylabel('{} ({})'.format(this_var['ctl'].varname,
-#     #                                             this_var['ctl'].units))
-#     #     ax[-1].set_xlabel('day of simulation')
-#     #     ax[0].set_title('{}'.format(this_site))
-#     #     ax[0].legend(loc='best')
-#     #     fig.tight_layout()
-#     #     fig.savefig(os.path.join(os.getenv('CSCRATCH'),
-#     #                              'plots_4year',
-#     #                              '{}.pdf'.format(this_site.replace(' ', ''))))
-#     #     # plt.show()
-
-
 if __name__ == "__main__":
     data_dir = os.path.join(os.getenv('CSCRATCH'), 'monthly_means')
     runs = ['CTL', 'IDE']
@@ -158,9 +110,10 @@ if __name__ == "__main__":
     sp_info = IDE_locations.CLMf05g16_get_spatial_info()
     domain = sp_info[0]
     locs = sp_info[1:]
+    cal_locs = locs[0:7]
     data = Vividict()
     units = Vividict()
-    for this_loc in locs:
+    for this_loc in cal_locs:
         sys.stdout.write('reading {}\n'.format(this_loc.name))
         for this_run in runs:
             sys.stdout.write('    {}: '.format(this_run))
@@ -183,7 +136,7 @@ if __name__ == "__main__":
         sys.stdout.write('{} '.format(v))
         sys.stdout.flush()
         df = pd.concat([data[r][loc.name][v].data
-                        for r in runs for loc in locs])
+                        for r in runs for loc in cal_locs])
         df['fyear'] = df['time'] / 365.0
         with sns.axes_style("white"):
             g = sns.FacetGrid(df, hue='case', palette="Dark2",
