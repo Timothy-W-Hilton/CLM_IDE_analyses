f117f10330676b6cd61f7febca40213e2a14547a
Timothy W. Hilton <thilton@ucmerced.edu>
2017-02-23 08:41:47 -0700

    implementing california PFT maps
    - this commit successfully plots the six most commno california PFTs.
      Next, need to show the total coverage of these six PFTs.

--- pft_mapper.py
+++ pft_mapper.py
@@ -8,6 +8,97 @@ from cartopy.feature import NaturalEarthFeature
 from RegionTester.region_tester import InUSState
 from timutils import colormap_nlevs
 
+
+class Cal6PanelMap(object):
+    """setup a six-panel map of california in two rows of three panels
+    with a horizontal colorbar at the bottom
+    """
+
+    def __init__(self, vmin, vmax, ncolorlevs=10, colormap=None):
+        self.vmin = vmin
+        self.vmax = vmax
+        if colormap is None:
+            self.base_colormap = plt.get_cmap('Blues')
+        else:
+            self.base_colormap = colormap
+        self.ncolorlevs = ncolorlevs
+        self.fig = None
+        self.projcal = ccrs.LambertConformal(central_longitude=262.5,
+                                             central_latitude=38.5,
+                                             standard_parallels=(38.5, 38.5))
+        self.idx = 0
+        self.__setup_axes()
+        self.__setup_maps()
+        self.__set_cmap_norm()
+
+    def __setup_axes(self):
+        """create axes for two-panel plot with colorbar at bottom
+        """
+        self.fig = plt.figure(figsize=(8, 8))
+        self.mapdict = {'map11':  plt.subplot2grid((120, 17), (0, 0),
+                                                   colspan=5, rowspan=50,
+                                                   projection=self.projcal),
+                        'map12': plt.subplot2grid((120, 17), (0, 6),
+                                                  colspan=5, rowspan=50,
+                                                  projection=self.projcal),
+                        'map13': plt.subplot2grid((120, 17), (0, 12),
+                                                  colspan=5, rowspan=50,
+                                                  projection=self.projcal),
+                        'map21': plt.subplot2grid((120, 17), (52, 0),
+                                                  colspan=5, rowspan=50,
+                                                  projection=self.projcal),
+                        'map22': plt.subplot2grid((120, 17), (52, 6),
+                                                  colspan=5, rowspan=50,
+                                                  projection=self.projcal),
+                        'map23': plt.subplot2grid((120, 17), (52, 12),
+                                                  colspan=5, rowspan=50,
+                                                  projection=self.projcal)}
+        self.axcbar = plt.subplot2grid((120, 17), (104, 0),
+                                       colspan=16, rowspan=8)
+
+    def __setup_maps(self):
+        """basic maps with parallels, meridians, coastlines, U.S. states
+        """
+        states = NaturalEarthFeature(category='cultural',
+                                     scale='50m',
+                                     facecolor='none',
+                                     name='admin_1_states_provinces_shp')
+        for this_map in self.mapdict.itervalues():
+            this_map.coastlines()
+            this_map.gridlines(draw_labels=False)
+            this_map.set_extent((-122, -114, 32, 43))
+            this_map.add_feature(states)
+
+    def __set_cmap_norm(self):
+        """get colormap, normalizer
+
+        """
+        self.cmap, self.norm = colormap_nlevs.setup_colormap(
+            vmin=self.vmin,
+            vmax=self.vmax,
+            nlevs=self.ncolorlevs,
+            cmap=self.base_colormap,
+            extend='neither')
+
+    def map_pft(self, pft_data, idx):
+        """plot grid cell percentage for one PFT on a map
+
+        ARGS:
+        pft_data: a PFTData object
+        """
+        this_ax = self.mapdict.values()[idx]
+        cm = this_ax.pcolormesh(pft_data.lon,
+                                pft_data.lat,
+                                pft_data.pft_data,
+                                transform=ccrs.PlateCarree(),
+                                cmap=self.cmap,
+                                norm=self.norm)
+        plt.colorbar(cm, cax=self.axcbar, orientation='horizontal')
+        self.axcbar.set_title('percentage', {'fontsize': 10})
+        this_ax.set_title(pft_data.pft_name.replace('_', ' '),
+                          {'fontsize': 10})
+
+
 class IDEPaperMap(object):
     """setup a two-panel map with horizonal colorbar
 
@@ -32,7 +123,7 @@ class IDEPaperMap(object):
         self.projglobe = ccrs.Miller()
         self.__setup_axes()
         self.__setup_maps()
-        self.__get_cmap_norm()
+        self.__set_cmap_norm()
 
     def __setup_axes(self):
         """create axes for two-panel plot with colorbar at bottom
@@ -62,7 +153,7 @@ class IDEPaperMap(object):
                                      name='admin_1_states_provinces_shp')
         self.mapcal.add_feature(states)
 
-    def __get_cmap_norm(self):
+    def __set_cmap_norm(self):
         """get colormap, normalizer
 
         """
@@ -86,9 +177,9 @@ class IDEPaperMap(object):
                                     transform=ccrs.PlateCarree(),
                                     cmap=self.cmap,
                                     norm=self.norm)
+            thismap.set_title('PFT: ' + pft_data.pft_name.replace('_', ' '))
         plt.colorbar(cm, cax=self.axcbar, orientation='horizontal')
         self.axcbar.set_title('percentage')
-        self.fig.suptitle('PFT: ' + pft_data.pft_name.replace('_', ' '))
 
     def draw_sites(self):
         """draw IDE site locations to map
@@ -150,7 +241,7 @@ class PFTData(object):
 
 def map_all_PFTs(fname_pft_names, fname_pft_data):
     map = IDEPaperMap(vmin=0.0, vmax=100.0, ncolorlevs=11)
-    for this_pft_idx in np.arange(2):  # there are 17 PFTs
+    for this_pft_idx in np.arange(17):  # there are 17 PFTs
         print 'mapping PFT {:02d}'.format(this_pft_idx)
         try:
             this_pft = PFTData(fname_pft_data, fname_pft_names, this_pft_idx)
@@ -168,4 +259,26 @@ if __name__ == "__main__":
                                    'CLM_Output',
                                    'pft-physiology.clm40.c130424.nc')
     fname_pft_data = '/Users/tim/work/Data/CLM_Output/CLM_PFTs.nc'
-    map_all_PFTs(fname_pft_names, fname_pft_data)
+    # map_all_PFTs(fname_pft_names, fname_pft_data)
+    cal_6panels = Cal6PanelMap(vmin=0.0, vmax=100.0, ncolorlevs=11)
+    ax_idx = 0
+    # these PFTs have significant presence in California
+    california_pfts = (0, 1, 2, 10, 13, 15)
+    pct_sum = None
+    for this_pft_idx in california_pfts:
+        print 'mapping PFT {:02d}'.format(this_pft_idx)
+        try:
+            this_pft = PFTData(fname_pft_data, fname_pft_names, this_pft_idx)
+            cal_6panels.map_pft(this_pft, ax_idx)
+            # track the total coverage of california PFTs.  This to
+            # make sure we get >= 90% of the plants in California,
+            # because there are a few other FPTs that appear in
+            # California in small areas.
+            if pct_sum is None:
+                pct_sum = this_pft.pft_data
+            else:
+                pct_sum += this_pft.pft_data
+        except:
+            print "error mapping PFT {:02d}".format(this_pft_idx)
+            raise
+        ax_idx += 1
