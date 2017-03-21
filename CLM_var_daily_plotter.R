ef24dc369040f7282200198e9bfd5ad9ebdc0ae4
Timothy W. Hilton <thilton@ucmerced.edu>
2017-03-20 21:01:12 -0600

    plot envelopes for 5, 95 percentile, use nice colors

--- CLM_var_daily_plotter.R
+++ CLM_var_daily_plotter.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(boot)
 
 df <- read.csv('./btran_daily.csv', header=TRUE)
 by_run <- group_by(select(df, case, loc, doy, value),
@@ -8,10 +9,15 @@ s <- summarize(by_run,
                count=n(),
                BTRAN=mean(value),
                minval=min(value),
-               maxval=max(value))
+               maxval=max(value),
+               pctl05=(quantile(value, 0.05)),
+               pctl95=(quantile(value, 0.95)))
+group_colors <- c(control='#8c510a', drought='#01665e')
+levels(s$case) <- c('control', 'drought')
 h <- ggplot(s, aes(doy, BTRAN, group=case, col=case)) +
-    geom_ribbon(aes(ymin = minval, ymax = maxval), fill="grey50", alpha=0.4) +
-        geom_line(aes(y = BTRAN), linetype=2)
+    geom_ribbon(aes(ymin = pctl05, ymax = pctl95), fill="grey50", alpha=0.4) +
+    geom_line(aes(y = BTRAN), linetype=2) +
+    scale_colour_manual(values=group_colors)
 h2 <- ggplot(filter(s, case=='IDE_redpcp'), aes(doy, BTRAN)) +
     geom_ribbon(aes(ymin = minval, ymax = maxval), fill="grey70") +
         geom_line(aes(y = BTRAN))
