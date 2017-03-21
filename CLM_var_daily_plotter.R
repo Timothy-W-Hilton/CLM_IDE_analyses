e8c27a8f463579b28e5fa84284f206ebb987bf77
Timothy W. Hilton <thilton@ucmerced.edu>
2017-03-21 11:26:29 -0600

    implement bootstrapped 95% CIs for daily BTRAN plots
    - 95% CIs: 95% confidence intervals
    
    - also remove unused code

--- CLM_var_daily_plotter.R
+++ CLM_var_daily_plotter.R
@@ -2,6 +2,27 @@ library(ggplot2)
 library(dplyr)
 library(boot)
 
+bootThetaMean <- function(x,i) {
+    mean(x[i])
+}
+
+boot_5_95 <- function(vals, R=1000) {
+    if (length(unique(vals)) > 1) {
+        b <- boot(vals, R=1000, statistic=bootThetaMean)
+        bci <- boot.ci(b, type='basic')
+        ## see boot.ci documentation: basic[4:5] contain the
+        ## confidence interval bounds (cilo = "CI low", cihi = "CI
+        ## high")
+        cilo <- bci[['basic']][4]
+        cihi <- bci[['basic']][5]
+        ci <- c(cilo=cilo, cihi=cihi)
+    } else {
+        unique_val <- unique(vals)
+        ci <- c(cilo=unique_val, cihi=unique_val)
+    }
+    return(ci)
+}
+
 df <- read.csv('./btran_daily.csv', header=TRUE)
 by_run <- group_by(select(df, case, loc, doy, value),
                    case, doy)
@@ -10,24 +31,16 @@ s <- summarize(by_run,
                BTRAN=mean(value),
                minval=min(value),
                maxval=max(value),
-               pctl05=(quantile(value, 0.05)),
-               pctl95=(quantile(value, 0.95)))
+               ci=list(boot_5_95(value, R=5)))  ## needs a single value
+s[['cilo']] <- unlist(lapply(s[['ci']], function(x) x[['cilo']]))
+s[['cihi']] <- unlist(lapply(s[['ci']], function(x) x[['cihi']]))
+s <- select(s, case, doy, count, BTRAN, minval, maxval, cilo, cihi)
+
+
 group_colors <- c(control='#8c510a', drought='#01665e')
 levels(s$case) <- c('control', 'drought')
 h <- ggplot(s, aes(doy, BTRAN, group=case, col=case)) +
-    geom_ribbon(aes(ymin = pctl05, ymax = pctl95), fill="grey50", alpha=0.4) +
+    geom_ribbon(aes(ymin = cilo, ymax = cihi),
+                fill="grey50", alpha=0.4) +
     geom_line(aes(y = BTRAN), linetype=2) +
     scale_colour_manual(values=group_colors)
-h2 <- ggplot(filter(s, case=='IDE_redpcp'), aes(doy, BTRAN)) +
-    geom_ribbon(aes(ymin = minval, ymax = maxval), fill="grey70") +
-        geom_line(aes(y = BTRAN))
-## question: how to plot cases separately?
-
-
-
-
-## huron <- data.frame(year = 1875:1972, level = as.vector(LakeHuron))
-## h <- ggplot(huron, aes(year))
-## h +
-##   geom_ribbon(aes(ymin = level - 1, ymax = level + 1), fill = "grey70") +
-##   geom_line(aes(y = level))
