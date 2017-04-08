65e9ac55726add7b8e3cfda13e5151a90dcd8398
Timothy W. Hilton <thilton@ucmerced.edu>
2017-04-07 22:09:33 -0700

    make CLM run and variable command line args

--- clm_2_daily.sh
+++ clm_2_daily.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#SBATCH -p debug
+#SBATCH -p regular
 #SBATCH -N 1
-#SBATCH -t 00:03:00
+#SBATCH -t 01:00:00
 #SBATCH -L cscratch1
 
-export VARNAME='FPSN'
-export RUNNAME='redpcp'
-export INDIR="/global/cscratch1/sd/twhilton/daily_CLM_output/input"
-# export INDIR="/global/cscratch1/sd/twhilton/archive/IDE_$RUNNAME/lnd/hist/"
-export OUTDIR="/global/cscratch1/sd/twhilton/daily_CLM_output/tmp"
+export VARNAME="$1"
+export RUNNAME="$2"
+# export INDIR="/global/cscratch1/sd/twhilton/daily_CLM_output/input"
+export INDIR="/global/cscratch1/sd/twhilton/archive/IDE_$RUNNAME/lnd/hist/"
+export OUTDIR="/global/cscratch1/sd/twhilton/daily_CLM_output/output"
 export FNAME_TMP="$OUTDIR/IDE_${RUNNAME}_6hrly_${VARNAME}.nc"
 export FNAME_FINAL="$OUTDIR/IDE_${RUNNAME}_daily_${VARNAME}.nc"
 
@@ -17,7 +17,8 @@ module load nco
 echo 'starting' `date`
 echo "$FNAME_TMP $FNAME_FINAL"
 rm -fv $FNAME_TMP $FNAME_FINAL
-ncrcat -v $VARNAME $INDIR/IDE_${RUNNAME}.clm2.h1.0001-01-0* $FNAME_TMP
+ncrcat -v $VARNAME $INDIR/IDE_${RUNNAME}.clm2.h1.* $FNAME_TMP
+# ncrcat -v $VARNAME $INDIR/IDE_${RUNNAME}.clm2.h1.0001-01-0* $FNAME_TMP
 ncra -O --mro -d time,1,,4,4 $FNAME_TMP $FNAME_FINAL
 rm -fv $FNAME_TMP
 echo 'done' `date`
