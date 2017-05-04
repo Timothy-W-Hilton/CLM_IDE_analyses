43e93eaed59b42b84408de01793bb145cff66b27
Timothy W. Hilton <thilton@ucmerced.edu>
2017-05-04 10:14:42 -0700

    change color scale of delta FPSN map to show Sahara values are 0.0

--- map_drought_var_magnitude_change.py
+++ map_drought_var_magnitude_change.py
@@ -68,9 +68,9 @@ if __name__ == "__main__":
     wcm_mag.plot(data=d_mag,
                  lon=varobjs['ctl'].lon,
                  lat=varobjs['ctl'].lat,
-                 vmin=d_mag.min(), vmax=5.0,
+                 vmin=d_mag.min(), vmax=0.2,
                  midpoint=0.0,
-                 bands_above=2,
+                 bands_above=3,
                  bands_below=8,
                  cmap_arg=plt.get_cmap('PuOr'),
                  cbar_tstr=(r'$\Delta$ annual max (drought - control)'
@@ -78,32 +78,32 @@ if __name__ == "__main__":
                  locations=locs)
     wcm_mag.fig.savefig('FPSN_Delta_ann_max.png')
 
-    wcm_mag_pct = WorldCalMap()
-    wcm_mag_pct.plot(data=d_mag_pct,
-                     lon=varobjs['ctl'].lon,
-                     lat=varobjs['ctl'].lat,
-                     vmin=-100.0, vmax=25.0,
-                     midpoint=0.0,
-                     bands_above=3,
-                     bands_below=10,
-                     cmap_arg=plt.get_cmap('PuOr'),
-                     cbar_tstr=(r'percent $\Delta$ annual max'
-                                ' (drought - controll)'),
-                     locations=locs)
-    wcm_mag_pct.fig.savefig('FPSN_PctDelta_ann_max.png')
+    # wcm_mag_pct = WorldCalMap()
+    # wcm_mag_pct.plot(data=d_mag_pct,
+    #                  lon=varobjs['ctl'].lon,
+    #                  lat=varobjs['ctl'].lat,
+    #                  vmin=-100.0, vmax=25.0,
+    #                  midpoint=0.0,
+    #                  bands_above=3,
+    #                  bands_below=10,
+    #                  cmap_arg=plt.get_cmap('PuOr'),
+    #                  cbar_tstr=(r'percent $\Delta$ annual max'
+    #                             ' (drought - controll)'),
+    #                  locations=locs)
+    # wcm_mag_pct.fig.savefig('FPSN_PctDelta_ann_max.png')
 
-    wcm_doy = WorldCalMap()
-    wcm_doy.plot(data=d_doy,
-                 lon=varobjs['ctl'].lon,
-                 lat=varobjs['ctl'].lat,
-                 vmin=-50, vmax=50,
-                 bands_above=7,
-                 bands_below=7,
-                 cmap_arg=plt.get_cmap('PuOr'),
-                 cbar_tstr=r'$\Delta$ DOY max (drought - control) [days]',
-                 locations=locs)
-    wcm_doy.fig.savefig('FPSN_DeltaDOY_ann_max.png')
+    # wcm_doy = WorldCalMap()
+    # wcm_doy.plot(data=d_doy,
+    #              lon=varobjs['ctl'].lon,
+    #              lat=varobjs['ctl'].lat,
+    #              vmin=-50, vmax=50,
+    #              bands_above=7,
+    #              bands_below=7,
+    #              cmap_arg=plt.get_cmap('PuOr'),
+    #              cbar_tstr=r'$\Delta$ DOY max (drought - control) [days]',
+    #              locations=locs)
+    # wcm_doy.fig.savefig('FPSN_DeltaDOY_ann_max.png')
 
-    fig = plt.figure()
-    plt.plot(varobjs['redpcp'].data[:, locs[-1].clm_y, locs[-1].clm_x])
-    plt.plot(varobjs['ctl'].data[:, locs[-1].clm_y, locs[-1].clm_x])
+    # fig = plt.figure()
+    # plt.plot(varobjs['redpcp'].data[:, locs[-1].clm_y, locs[-1].clm_x])
+    # plt.plot(varobjs['ctl'].data[:, locs[-1].clm_y, locs[-1].clm_x])
